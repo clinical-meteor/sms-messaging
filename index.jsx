f2b000fb3a8697661a33d70d483e8e494c249ac0
Abigail Watson <abigailwatson@Abigails-Mac-mini-317685.local>
2018-04-15 11:40:49 -0500

    conditional use of clinical:fhir-vault-server

--- index.jsx
+++ index.jsx
@@ -3,18 +3,14 @@ import PostcardPage from './client/PostcardPage.jsx';
 
 var DynamicRoutes = [{
   'name': 'HelloWorldPage',
-  'path': '/hello-world',
+  'path': '/sms-messaging',
   'component': HelloWorldPage
-}, {
-  'name': 'SampleRoute',
-  'path': '/postcard',
-  'component': PostcardPage
 }];
 
 var SidebarElements = [{
-  'primaryText': 'Example Page',
-  'to': '/hello-world',
-  'href': '/hello-world'
+  'primaryText': 'SMS Messaging',
+  'to': '/sms-messaging',
+  'href': '/sms-messaging'
 }];
 
 export { SidebarElements, DynamicRoutes, SamplePage, PostcardPage };
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'clinical:sms-messaging',
-    version: '0.0.1',
+    version: '0.0.2',
     summary: 'SMS Messaging - Alerts, notifications, 2 factor authentication, patient engagement, etc.',
     git: 'https://github.com/clinical-meteor/example-plugin',
     documentation: 'README.md'
@@ -17,8 +17,10 @@ Package.onUse(function(api) {
 
     api.use('clinical:glass-ui@2.1.6');
     api.use('clinical:base-model@1.3.5');
-    api.use('clinical:fhir-vault-server@0.0.3', ['client', 'server'], {weak: true});
- 
+    if(Package['clinical:fhir-vault-server']){
+        api.use('clinical:fhir-vault-server@0.0.3', ['client', 'server'], {weak: true});
+    }
+     
     api.use('aldeed:simple-schema@1.3.3');
     api.use('aldeed:collection2@2.5.0');
     api.use('simple:json-routes@2.1.0');
